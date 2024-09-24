6c038c2a7900a94a7a50a9e97aa77a9fef201cf9
Scott Beamer <sbeamer@ucsc.edu>
2024-09-24 11:01:59 -0700

    add marcelo to pub crawler

--- scripts/dblp_crawler.py
+++ scripts/dblp_crawler.py
@@ -9,7 +9,7 @@ from xml.dom import minidom
 from collections import defaultdict
 
 # Name of the authors
-professors = {'Jose Renau', 'Heiner Litz', 'Matthew R.Guthaus', 'Scott Beamer', 'Dustin Richmond', 'Tyler Sorensen', 'Jason Eshraghian', 'Colleen Josephson', 'Yuanchao Xu 0001'}
+professors = {'Jose Renau', 'Heiner Litz', 'Matthew R.Guthaus', 'Scott Beamer', 'Dustin Richmond', 'Tyler Sorensen', 'Jason Eshraghian', 'Colleen Josephson', 'Yuanchao Xu 0001', 'Marcelo Orenes-Vera'}
 
 
 # Get DBLP's key
